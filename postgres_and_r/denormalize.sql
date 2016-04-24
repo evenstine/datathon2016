04f3e687278108d7dc14e180ae214d29150148ac
esteban <juanestebanmt@gmail.com>
2016-04-24 19:59:13 +1000

    working on query to denormanize clickstream data

--- postgres_and_r/denormalize.sql
+++ postgres_and_r/denormalize.sql
@@ -1,36 +1,60 @@
-select 
-	j.job_id,
-	j.title,
-	j.abstract,
-	j.hat,
-	j.salary_min,
-	j.salary_max,
-	j.raw_job_type,
-	ji.session_id,
-	js.search_id,
-	js.user_id, 
-	js.query,
-	jc.user_id,
-	ji.search_ranking,
-	js.raw_location as job_search_location,
-	j.raw_location as job_location,
-	jclass.class_id,
-	jclass.class_description,
-	jclass.sub_class_description,
-	js.mobile_user,
-	j.segment,
-	js.created_at as job_search_created,
-	ji.created_at as job_impression_created,
-	jc.created_at as job_click_created
+drop table if exists classified_jobs;
+
+create table classified_jobs as 
+	select 
+		j.*, 
+		c.class_description, 
+		c.sub_class_description 
+	from 
+		jobs_all as j 
+	left join 
+		job_classification as c 
+	on 
+		c.sub_class_id = j.subclasses;
+
+create index classified_jobs_job_id_index on classified_jobs (job_id);
+analyse classified_jobs;
+
+select
+	s.user_id,
+	s.search_id,
+	s.raw_location,
+	s.location_id,
+	s.latitude,
+	s.longitude,
+	s.query,
+	s.mobile_user,
+	s.created_at as time_of_search,
+	'impression' as event_type,
+	i.job_id,
+	i.session_id,
+	i.search_ranking,
+	i.created_at as time_of_impresion
 from
-	jobs_all j,
-	jobs_impressions_all ji,
-	job_searches_all js,
-	job_clicks_all jc,
-	job_classification jclass
-where
-	j.job_id = ji.job_id
-	and jc.job_id = j.job_id
-	and jc.user_id = js.user_id
-	and jc.search_id = js.search_id
-	and (j.subclasses = -1 or j.subclasses = jclass.sub_class_id)
+	job_searches_all as s
+left join
+	job_impressions_all as i
+on
+	s.search_id = i.search_id and s.user_id = i.user_id
+union
+select
+	s.user_id,
+	s.search_id,
+	s.raw_location,
+	s.location_id,
+	s.latitude,
+	s.longitude,
+	s.query,
+	s.mobile_user,
+	s.created_at as time_of_search,
+	'click' as event_type,
+	c.job_id,
+	c.session_id,
+	null as search_ranking,
+	c.created_at as time_of_click
+from
+	job_searches_all as s
+left join
+	job_clicks_all as c
+on
+	s.search_id = c.search_id and s.user_id = c.user_id
