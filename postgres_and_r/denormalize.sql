a3f7945afda0c0a857cc7939f9deff3282444bff
esteban <juanestebanmt@gmail.com>
2016-04-25 17:02:57 +1000

    now the search clicks take account for impressions and clicks which do not have a matching search

--- postgres_and_r/denormalize.sql
+++ postgres_and_r/denormalize.sql
@@ -29,10 +29,11 @@ select
 	s.mobile_user,
 	s.created_at as time_of_search,
 	case when i.job_id is null then 'no-results'::text else 'impression'::text end  as event_type,
+	i.search_id as event_search_id,
 	i.job_id,
 	i.session_id,
 	i.search_ranking,
-	i.created_at as time_of_impresion
+	i.created_at as time_of_event
 from
 	job_searches_all as s
 left join
@@ -51,16 +52,67 @@ select
 	s.mobile_user,
 	s.created_at as time_of_search,
 	case when c.job_id is null then 'no-results'::text else 'click'::text end  as event_type,
+	c.search_id as event_search_id,
 	c.job_id,
 	c.session_id,
 	null as search_ranking,
-	c.created_at as time_of_click
+	c.created_at as time_of_event
 from
 	job_searches_all as s
 left join
 	job_clicks_all as c
 on
-	s.search_id = c.search_id and s.user_id = c.user_id;
+	s.search_id = c.search_id and s.user_id = c.user_id
+union
+select
+	s.user_id,
+	s.search_id,
+	s.raw_location,
+	s.location_id,
+	s.latitude,
+	s.longitude,
+	s.query,
+	s.mobile_user,
+	s.created_at as time_of_search,
+	'impression'::text as event_type,
+	i.search_id as event_search_id,
+	i.job_id,
+	i.session_id,
+	i.search_ranking,
+	i.created_at as time_of_event
+from
+	job_searches_all as s
+right join
+	job_impressions_all as i
+on
+	s.search_id = i.search_id and s.user_id = i.user_id
+where 
+	s.search_id is null
+union
+select
+	s.user_id,
+	s.search_id,
+	s.raw_location,
+	s.location_id,
+	s.latitude,
+	s.longitude,
+	s.query,
+	s.mobile_user,
+	s.created_at as time_of_search,
+	case when c.job_id is null then 'no-results'::text else 'click'::text end  as event_type,
+	c.search_id as event_search_id,
+	c.job_id,
+	c.session_id,
+	null as search_ranking,
+	c.created_at as time_of_event
+from
+	job_searches_all as s
+left join
+	job_clicks_all as c
+on
+	s.search_id = c.search_id and s.user_id = c.user_id
+where
+	s.search_id is null;
 
 create index search_impressions_clicks_search_id_index on search_impressions_clicks(search_id);
 create index search_impressions_clicks_job_id_index on search_impressions_clicks(job_id);
