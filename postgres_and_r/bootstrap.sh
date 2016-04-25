986693476ae839244ce32b33a6803af57e3ed829
esteban <juanestebanmt@gmail.com>
2016-04-25 19:28:04 +1000

    re-organizing the code

--- postgres_and_r/bootstrap.sh
+++ postgres_and_r/bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-psql -d datathone2016 -f load_data_to_postgres.sql
-psql -d datathone2016 -f denormalize.sql
+psql -d datathone2016 -f ./sql/load_data_to_postgres.sql
+psql -d datathone2016 -f ./sql/denormalize.sql
